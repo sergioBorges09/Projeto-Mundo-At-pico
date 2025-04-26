339595517a238de68f195cdd933a92f384d46714
Sergio Rodrigues Borges <sergiorodriguessjm67@gmail.com>
2025-04-26 11:02:00 -0300

    Mexendo na parte conexão com back end

---
+++ java-script/integracao.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.get("http://localhost:8080/medicos").then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
